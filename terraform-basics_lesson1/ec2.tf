ba2c85e4026dd99bfe616083d92a5d2f880b6924
Jens Maes <mail@ditisjens.be>
2021-11-18 12:10:58 +0100

    - Added user data + SSH key in terraform launch script. - Created launch script for Apache2

--- terraform-basics_lesson1/ec2.tf
+++ terraform-basics_lesson1/ec2.tf
@@ -1,16 +1,28 @@
+# Creates an EC2 instance in eu-west-1, in subnet 1a with the 2 security groups
+
 provider "aws" {
+
 	region = "eu-west-1"
+
 }
 
 resource "aws_instance" "cloud-example-3" {
-	ami = "ami-016ee74f2cf016914"
-	instance_type = "t3.micro"
+	ami = "ami-08edbb0e85d6a0a07"
+	instance_type = "t2.micro"
 	subnet_id = var.vpc-subnet-1a
 	vpc_security_group_ids = [
+
 		"sg-067833e558096224a",
 		"sg-0010525517c718fb0"
+
 	]
+	user_data = "${file("install-files/apache2.sh")}"
 	tags = {
+
 	  "Name" = "terraform-instance-4"
+
 	}
+
+	# Allows us to connect using our currently available AWS SSH key
+	key_name = "JensMaes"
 }
