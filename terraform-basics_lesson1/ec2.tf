bfa7fed4698ccfc464a12e31b03730235f510618
Jens Maes <mail@ditisjens.be>
2021-11-18 11:31:19 +0100

    Test

--- terraform-basics_lesson1/ec2.tf
+++ terraform-basics_lesson1/ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
 	region = "eu-west-1"
 }
 
-resource "aws_instance" "cloud-example" {
+resource "aws_instance" "cloud-example-3" {
 	ami = "ami-016ee74f2cf016914"
 	instance_type = "t3.micro"
 	subnet_id = var.vpc-subnet-1a
@@ -11,6 +11,6 @@ resource "aws_instance" "cloud-example" {
 		"sg-0010525517c718fb0"
 	]
 	tags = {
-	  "Name" = "terraform-instance-3"
+	  "Name" = "terraform-instance-4"
 	}
 }
